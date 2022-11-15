262e2825b4d11443409decc64cd4bc2e2c07250a
JordanRendon <jordanre7991@gmail.com>
2022-11-15 09:58:23 -0500

    Desplegando Quiz#2

---
+++ src/components/ApiFakestore/ApiFakestore.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import './ApiFakestore.css'
+
+
+const ApiFakestore = () => {
+    const [productos, setProductos] = useState([]);
+
+    const fetchProductos = async () =>{
+        const response = await axios.get('https://fakestoreapi.com/products')
+        setProductos(response.data)
+    }
+
+    useEffect(()=>{
+        fetchProductos()
+    }, [])
+  return (
+    <div className='conteiner-padre'>
+        {/* <div className='container-productos'> */}
+            {productos.map((producto)=>(
+                <div key={producto.id} className='item-producto'>
+                    <h2 className='title'>{producto.title}</h2>
+                    <img src={producto.image} alt="" />
+                    <p>{producto.description}</p>
+                    <h2>Category: ${producto.category}</h2>
+                    <h2>Price: ${producto.price}</h2>
+                </div>
+            ))}
+        {/* </div> */}
+    </div>
+  )
+}
+
+export default ApiFakestore
